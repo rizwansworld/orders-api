139f1a51df09ee0520aa6c5b4b2f4780f415d3a1
Rizwan Aboobacker Sideeque <rizwansworld@Rizwans-MacBook-Pro.local>
2024-04-19 18:29:42 +0100

    Graceful shutdown added

--- application/app.go
+++ application/app.go
@@ -4,15 +4,20 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -24,11 +29,37 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		// Error Wrapping
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
-	return nil
+	// Graceful shutdown implemented
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			// Error Wrapping
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/rizwansworld/orders-api/application"
 )
@@ -10,7 +12,16 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	// The NotifyContext method returns two things
+	// A derived context from a root context which can be used to cancel any ongoing tasks based on signal interrupt.
+	// A cancel function which can be called to perform the cancellations.
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	// defer keyword calls the cancel function at the end of the current function, as our requirement.
+	// In our case, end of the main function.
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
